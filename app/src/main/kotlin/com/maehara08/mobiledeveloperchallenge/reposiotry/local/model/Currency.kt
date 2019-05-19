d436357fb820e068b6d64460501a659391b73dee
maehara08 <aso.riiiku08@gmail.com>
2019-05-19 22:45:36 +0900

    Add to method.

--- app/src/main/kotlin/com/maehara08/mobiledeveloperchallenge/reposiotry/local/model/Currency.kt
+++ app/src/main/kotlin/com/maehara08/mobiledeveloperchallenge/reposiotry/local/model/Currency.kt
@@ -1,6 +1,11 @@
 package com.maehara08.mobiledeveloperchallenge.reposiotry.local.model
 
+import com.maehara08.mobiledeveloperchallenge.reposiotry.local.entity.Currency
+
 data class Currency(
   val name: String,
   val description: String
 )
+
+fun Currency.toLocalCurrency() =
+  com.maehara08.mobiledeveloperchallenge.reposiotry.local.model.Currency(name, description)
