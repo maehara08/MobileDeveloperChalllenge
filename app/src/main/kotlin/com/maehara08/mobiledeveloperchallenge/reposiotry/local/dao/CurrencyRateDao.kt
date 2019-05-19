298a7678ce161e4fd28765dc233cfbaf75b0ed46
maehara08 <aso.riiiku08@gmail.com>
2019-05-20 00:25:40 +0900

    Add dao,

---
+++ app/src/main/kotlin/com/maehara08/mobiledeveloperchallenge/reposiotry/local/dao/CurrencyRateDao.kt
@@ -0,0 +1,20 @@
+package com.maehara08.mobiledeveloperchallenge.reposiotry.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.maehara08.mobiledeveloperchallenge.reposiotry.local.entity.CurrencyRate
+
+@Dao
+interface CurrencyRateDao {
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  fun insertCurrencies(currencies: List<CurrencyRate>)
+
+  @Query("SELECT * FROM CurrencyRate")
+  fun findAll(): List<CurrencyRate>
+
+  @Delete
+  fun delete(currency: CurrencyRate)
+}
