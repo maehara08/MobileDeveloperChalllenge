07d86ddc7cb347fd406d44f0b77904bfa98091cd
maehara08 <aso.riiiku08@gmail.com>
2019-05-19 19:20:59 +0900

    Init db.

--- app/src/main/kotlin/com/maehara08/mobiledeveloperchallenge/ui/CurrencyApplication.kt
+++ app/src/main/kotlin/com/maehara08/mobiledeveloperchallenge/ui/CurrencyApplication.kt
@@ -1,13 +1,26 @@
 package com.maehara08.mobiledeveloperchallenge.ui
 
 import android.app.Application
+import androidx.room.Room
 import com.maehara08.mobiledeveloperchallenge.BuildConfig
 import com.maehara08.mobiledeveloperchallenge.reposiotry.CurrencyListRepository
+import com.maehara08.mobiledeveloperchallenge.reposiotry.local.DataBase
 import timber.log.Timber
 
 class CurrencyApplication : Application() {
+  companion object {
+    private const val DB_NAME = "my_db.db"
+  }
+
+  lateinit var dataBase: DataBase
+
   override fun onCreate() {
     super.onCreate()
+
+    //db
+    dataBase = Room.databaseBuilder(this, DataBase::class.java, DB_NAME)
+        .build()
+
     // Repository
     listOf(
         CurrencyListRepository
