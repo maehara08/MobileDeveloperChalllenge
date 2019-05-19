066079ef297cd99587bec7fac0fb00a8035773f1
maehara08 <aso.riiiku08@gmail.com>
2019-05-19 19:03:46 +0900

    Create vm instance.

--- app/src/main/kotlin/com/maehara08/mobiledeveloperchallenge/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/maehara08/mobiledeveloperchallenge/ui/main/MainActivity.kt
@@ -1,12 +1,21 @@
-package com.maehara08.mobiledeveloperchallenge
+package com.maehara08.mobiledeveloperchallenge.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import com.maehara08.mobiledeveloperchallenge.R
+import com.maehara08.mobiledeveloperchallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    val viewModel = ViewModelProviders.of(this)
+        .get(MainViewModel::class.java)
+    viewModel.getCurrencyList()
   }
 }
