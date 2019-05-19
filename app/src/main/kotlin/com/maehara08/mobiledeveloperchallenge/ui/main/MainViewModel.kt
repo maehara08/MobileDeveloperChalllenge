76455561ddae3c95cb2c2fa16b9193f1dddaccff
maehara08 <aso.riiiku08@gmail.com>
2019-05-19 19:03:28 +0900

    Create main vm.

---
+++ app/src/main/kotlin/com/maehara08/mobiledeveloperchallenge/ui/main/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.maehara08.mobiledeveloperchallenge.ui.main
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.maehara08.mobiledeveloperchallenge.reposiotry.CurrencyListRepository
+import com.maehara08.mobiledeveloperchallenge.reposiotry.local.Currency
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+  val currencyData = MutableLiveData<List<Currency>>()
+
+  fun getCurrencyList() {
+    viewModelScope.launch {
+      try {
+        val list = CurrencyListRepository.getList()
+            .await()
+        currencyData.postValue(list)
+      } catch (e: Exception) {
+        Timber.e(e)
+      }
+    }
+  }
+}
