fa8bcccb62d3a34473e299de3bdda7ea181904c7
maehara08 <aso.riiiku08@gmail.com>
2019-05-19 22:46:08 +0900

    Fix repo.

--- app/src/main/kotlin/com/maehara08/mobiledeveloperchallenge/reposiotry/CurrencyListRepository.kt
+++ app/src/main/kotlin/com/maehara08/mobiledeveloperchallenge/reposiotry/CurrencyListRepository.kt
@@ -2,11 +2,19 @@ package com.maehara08.mobiledeveloperchallenge.reposiotry
 
 import android.app.Application
 import com.maehara08.mobiledeveloperchallenge.BuildConfig
+import com.maehara08.mobiledeveloperchallenge.entension.isWithin30Minutes
 import com.maehara08.mobiledeveloperchallenge.reposiotry.local.DataBase
+import com.maehara08.mobiledeveloperchallenge.reposiotry.local.UpdatedAtPreference
+import com.maehara08.mobiledeveloperchallenge.reposiotry.local.entity.toEntityCurrency
 import com.maehara08.mobiledeveloperchallenge.reposiotry.local.model.Currency
+import com.maehara08.mobiledeveloperchallenge.reposiotry.local.model.toLocalCurrency
 import com.maehara08.mobiledeveloperchallenge.reposiotry.remote.service.CurrencyListService
 import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Date
 
 object CurrencyListRepository : Repository {
   private lateinit var application: Application
@@ -24,20 +32,38 @@ object CurrencyListRepository : Repository {
 
   suspend fun getList(): Deferred<List<Currency>> {
     val deferred = CompletableDeferred<List<Currency>>()
-    Network.request(
-        application,
-        currencyListService.getCurrencyList(BuildConfig.ACCESS_TOKEN),
-        {
-          val arrayList = arrayListOf<Currency>()
-          for (key in it.currencies.keySet()) {
-            val value = it.currencies[key].asString
-            arrayList.add(Currency(key, value))
-          }
-          deferred.complete(arrayList)
-        },
-        {
-          deferred.completeExceptionally(it)
-        })
+    CoroutineScope(Dispatchers.IO).launch {
+      val isWithin30Minutes = UpdatedAtPreference.currencyList.isWithin30Minutes()
+      if (!isWithin30Minutes) {
+        Network.request(
+            application,
+            currencyListService.getCurrencyList(BuildConfig.ACCESS_TOKEN),
+            {
+              val currencyList = arrayListOf<Currency>()
+              for (key in it.currencies.keySet()) {
+                val value = it.currencies[key].asString
+                currencyList.add(Currency(key, value))
+              }
+              deferred.complete(currencyList)
+              currencyList.map { currency ->
+                currency.toEntityCurrency()
+              }
+                  .let(database.currencyDao()::insertCurrencies)
+              UpdatedAtPreference.currencyList = Date()
+            },
+            {
+              deferred.completeExceptionally(it)
+            })
+      } else {
+        database.currencyDao()
+            .findAll()
+            .map {
+              it.toLocalCurrency()
+            }
+            .let(deferred::complete)
+      }
+    }
+        .join()
     return deferred
   }
 }
