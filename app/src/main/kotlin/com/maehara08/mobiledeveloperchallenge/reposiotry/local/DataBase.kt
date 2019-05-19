edf8fb849fc29402a7af82b54e729cb832d7a840
maehara08 <aso.riiiku08@gmail.com>
2019-05-20 00:28:45 +0900

    Add to db.

--- app/src/main/kotlin/com/maehara08/mobiledeveloperchallenge/reposiotry/local/DataBase.kt
+++ app/src/main/kotlin/com/maehara08/mobiledeveloperchallenge/reposiotry/local/DataBase.kt
@@ -3,9 +3,13 @@ package com.maehara08.mobiledeveloperchallenge.reposiotry.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.maehara08.mobiledeveloperchallenge.reposiotry.local.dao.CurrencyDao
+import com.maehara08.mobiledeveloperchallenge.reposiotry.local.dao.CurrencyRateDao
 import com.maehara08.mobiledeveloperchallenge.reposiotry.local.entity.Currency
+import com.maehara08.mobiledeveloperchallenge.reposiotry.local.entity.CurrencyRate
 
-@Database(entities = [Currency::class], version = 1, exportSchema = false)
+@Database(entities = [Currency::class, CurrencyRate::class], version = 1, exportSchema = false)
 abstract class DataBase : RoomDatabase() {
   abstract fun currencyDao(): CurrencyDao
+
+  abstract fun currencyRateDao(): CurrencyRateDao
 }
